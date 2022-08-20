212394822d3d096a5d253d8796a61c93050b8cc8
gioiascari <gioia.ascari96@gmail.com>
2022-08-21 00:38:58 +0200

    bugs fixed with api router

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Upcoming;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+//Upcoming Task
+Route::get('/upcoming', function(){
+    $upcoming = Upcoming::all();
+    return $upcoming;
 });
