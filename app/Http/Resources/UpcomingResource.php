fb906b9c1ceeeb2ff6f304debfac1988717efc76
gioiascari <gioia.ascari96@gmail.com>
2022-08-22 15:54:57 +0200

    created today and upcoming resources

--- app/Http/Resources/UpcomingResource.php
+++ app/Http/Resources/UpcomingResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TodayTaskResource extends JsonResource
+class UpcomingResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
